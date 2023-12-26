5eb2bff870d681d7a719e50596626de67b3abc33
Rakshyak Adhikari <rakshyak.adhikari@gmail.com>
2023-12-26 17:59:28 -0600

    learn basic css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
+import "./App.css";
+
 function App() {
-  return <img src="https://images.unsplash.com/photo-1522992319-0365e5f11656?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+  return <main>Hello</main>;
 }
 
-export default App
+export default App;
