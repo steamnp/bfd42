9ccea0944500ee3c72f2932b7af2d578e185bd47
Gorakh Raj Joshi <59221046+gorakhjoshi@users.noreply.github.com>
2023-12-27 08:02:03 +0545

    learn js variable and value

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import styles from './App.module.css'
 
 function App() {
-  return <main className={styles.box}>Hello</main>
+  // this is the location to write js code
+  // 1. Create variable and assign value to it
+  // const myName = 'Gorakh'
+  // let myName = 'Gorakh'
+  // myName = 'Jayanti'
+
+  // const myCountry = 'Nepal'
+
+  let age = 30
+  age = 31
+  age = 32
+
+  return <main className={styles.box}>{age}</main>
 }
 
 export default App
+
+// where can we write JavaScript code ->.jsx, .js, .tsx, .ts
