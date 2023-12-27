eb3b2fb20a863fceaf698d9c5453f46049fd7599
Sapna Tamang <tamangsapna08@gmail.com>
2023-12-26 20:32:18 -0600

    function

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import styles from "./App.module.css";
 function App() {
-  const myName = "Sapna";
-  return <main classname={styles.box}>{myName}</main>;
+  //const myName = "Sapna";
+  let age = 30;
+  age = 31;
+  console.log("Test");
+  function getAge() {
+    console.log("Hello");
+  }
+  getAge();
+  return <main classname={styles.box}>My age is {age}</main>;
 }
 
 export default App;
