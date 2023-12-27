f5b08b60108dd051567298d8e58265a39940d428
Sanjip K Sapkota <sapkotasanjipk@outlook.com>
2023-12-26 21:03:37 -0500

    const variable

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 function App() {
-  return <img src="https://images.unsplash.com/photo-1522992319-0365e5f11656?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+  const myName = 'Sanjip'
+  return (
+    <div>
+      <h1>{myName}</h1>
+  <img src="https://images.unsplash.com/photo-1522992319-0365e5f11656?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
   alt = "coffee on the table"
   style = {{width: '200px'}}/>
-}
+
+  </div>
+)}
 
 export default App
