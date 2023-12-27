6e50c125ef649b0eb7376d0a799e8045ad4b3547
Gorakh Raj Joshi <59221046+gorakhjoshi@users.noreply.github.com>
2023-12-27 08:53:30 +0545

    learn component in React

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import styles from './App.module.css'
 
 function App() {
+  // console.log('App')
   // this is the location to write js code
   // 1. Create variable and assign value to it
   // const myName = 'Gorakh'
@@ -13,7 +14,27 @@ function App() {
   age = 31
   age = 32
 
-  return <main className={styles.box}>{age}</main>
+  // console.log('Test')
+
+  // How to create function in JS
+  // 3 ways to create function in JS
+  // 1. Function Declaration
+  // (-> Parameter)
+  function getAge(birthDate) {
+    // console.log(age)
+    return 2023 - birthDate
+  }
+
+  // console.log('Greeting')
+
+  // Call/invoke/run function getAge
+  const myAge = getAge(1990)
+  const anotherAge = getAge(1996)
+
+  console.log(anotherAge)
+  // getAge(1990 -> Argument)
+
+  return <main className={styles.box}>{anotherAge}</main>
 }
 
 export default App
