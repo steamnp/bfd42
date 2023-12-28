7de8c77e0a7e839b9c276b999c36588f8f85142f
Sapna Tamang <tamangsapna08@gmail.com>
2023-12-27 20:43:39 -0600

    Import-Export

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,8 @@
 import styles from "./App.module.css";
+import country from "./utils/test";
+
 function App() {
-  //const myName = "Sapna";
-  let age = 30;
-  age = 31;
-  console.log("Test");
-  function getAge(birthDate) {
-    // console.log(age);
-    return 2023 - birthDate;
-  }
-  // getAge(30);
-  const myAge = getAge(1993);
-  return <main classname={styles.box}>{myAge}</main>;
+  return <main>{country}</main>;
 }
 
 export default App;
