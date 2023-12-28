4c3bc2a7f751fb65fdf29b494f2de61b284bd8dd
Rajip Bardewa <rajip@takeo.ai>
2023-12-29 03:07:53 +0545

    learn component in React

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,15 @@
 import styles from "./App.module.css";
 
+// Component
+// if name is following PascalCase convention
+// & if it is returning JSX
 function App() {
-  // console.log("App");
-  // this is the location to write js code
-  // 1. create variable and assign value to it
-  // const myName = "Rajip";
-  // let myName = "Gorakh";
-  // myName = "Jayanti";
-
-  // const myCountry = "Nepal"
-
-  let age = 30;
-  age = 31;
-  age = 32;
-
-  // console.log("Test");
-
-  // how to create function in JS
-  // 3 ways to create function in JS
-  //  1. Function Declaration
-  // (-> Parameter/paranthesis)
-  function getAge(birthDate) {
-    // console.log(age);
-    return 2023 - birthDate;
-  }
-
-  // console.log("Greetings");
-
-  // call/invoke/run function getAge
-  const result = getAge(1997);
-  const myAge = getAge(2000);
-
-  console.log(myAge);
-  // getAge(1997 -> Argument)
-
-  return <main className={StyleSheet.box}>{myAge}</main>;
+  return <main className={StyleSheet.box}>Hello</main>;
 }
 
 export default App;
+
+// function (generic JS function)
+// function getAge(){
+// return 20
+// }
