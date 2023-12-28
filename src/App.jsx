dff086b89477de3d8ce695af65ec4e15156e32c7
Rajip Bardewa <rajip@takeo.ai>
2023-12-29 02:50:28 +0545

    create getAge function

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import { country } from "./test";
+import styles from "./App.module.css";
 
 function App() {
+  // console.log("App");
   // this is the location to write js code
   // 1. create variable and assign value to it
   // const myName = "Rajip";
@@ -13,7 +14,27 @@ function App() {
   age = 31;
   age = 32;
 
-  return <main className={StyleSheet.box}>{age}</main>;
+  // console.log("Test");
+
+  // how to create function in JS
+  // 3 ways to create function in JS
+  //  1. Function Declaration
+  // (-> Parameter/paranthesis)
+  function getAge(birthDate) {
+    // console.log(age);
+    return 2023 - birthDate;
+  }
+
+  // console.log("Greetings");
+
+  // call/invoke/run function getAge
+  const result = getAge(1997);
+  const myAge = getAge(2000);
+
+  console.log(myAge);
+  // getAge(1997 -> Argument)
+
+  return <main className={StyleSheet.box}>{myAge}</main>;
 }
 
 export default App;
