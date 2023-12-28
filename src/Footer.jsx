3d415d2e947147e726f76d2cfb2b24bb0529e6d6
Hima Lama <hima.lama2009@gmail.com>
2023-12-27 22:09:06 -0800

    create module css and apply in header and footer component

---
+++ src/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from "./styles.module.css"
+function Footer() {
+  return (
+    <div className={styles.footer}>Footer</div>
+  )
+}
+
+export default Footer
---
+++ src/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from "./styles.module.css"
+function Header() {
+  return (
+    <div className={styles.header} >Header</div>
+  )
+}
+
+export default Header
