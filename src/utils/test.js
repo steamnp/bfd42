80a132a9d0fbd645a2c3f288067d1d40196062a6
Gorakh Raj Joshi <59221046+gorakhjoshi@users.noreply.github.com>
2023-12-28 08:27:30 +0545

    import and export

---
+++ src/utils/test.js
@@ -0,0 +1,8 @@
+import { anotherCountry } from '../variable'
+// named export
+// export const country = anotherCountry
+
+const country = anotherCountry
+
+// default export
+export default country
