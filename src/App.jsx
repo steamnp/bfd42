33abca3c21c7a7a79bb5b0b66ec59c3480ab8d2e
Gorakh Raj Joshi <59221046+gorakhjoshi@users.noreply.github.com>
2023-12-28 07:47:13 +0545

    introduce component in react

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,22 @@
 import styles from './App.module.css'
 
+// Component
+// if name is following PacalCase convention
+// & if it is returning JSX
 function App() {
-  // console.log('App')
-  // this is the location to write js code
-  // 1. Create variable and assign value to it
-  // const myName = 'Gorakh'
-  // let myName = 'Gorakh'
-  // myName = 'Jayanti'
-
-  // const myCountry = 'Nepal'
-
-  let age = 30
-  age = 31
-  age = 32
-
-  // console.log('Test')
-
-  // How to create function in JS
-  // 3 ways to create function in JS
-  // 1. Function Declaration
-  // (-> Parameter)
-  function getAge(birthDate) {
-    // console.log(age)
-    return 2023 - birthDate
-  }
-
-  // console.log('Greeting')
-
-  // Call/invoke/run function getAge
-  const myAge = getAge(1990)
-  const anotherAge = getAge(1996)
-
-  console.log(anotherAge)
-  // getAge(1990 -> Argument)
-
-  return <main className={styles.box}>{anotherAge}</main>
+  return <main className={styles.box}>Hello</main>
 }
 
 export default App
 
-// where can we write JavaScript code ->.jsx, .js, .tsx, .ts
+// function (generic JS function)
+// function getAge() {
+//   return 20
+// }
+
+// let, const -> 2015
+
+// ED6 -> 2015 -> Modern JS
+// ES6 modules
+// import
+// export
