b1d547dd36491933cf55e12e810ec24ddd5e495d
Lama <txl3311@gmail.com>
2023-12-28 19:52:52 -0600

    made new react program

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import styles from "./App.module.css";
+import sum from "./utils/number";
 
 function App() {
-  return <main className={styles.box}>Hello </main>;
-  // <img
-  //   src="https://images.unsplash.com/photo-1522992319-0365e5f11656?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-  //   style={{ width: "200px" }}
+  return;
+  <sum />;
 }
 
 export default App;
