627bb9a1e841c63b9c499564aa189fee7795f35a
sushant lamichhane <lamichhanesushant12@gmail.com>
2023-12-28 18:04:13 -0800

    get some js code

---
+++ src/utils/number.js
@@ -0,0 +1,5 @@
+function getSum(firstNum, secondNum) {
+  return firstNum + secondNum;
+}
+
+export default getSum;
---
+++ src/utils/pages/home/home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HomeComponent = () => {
+  return (
+    <div>
+      <h1>Welcome to our Website</h1>
+    </div>
+  );
+};
+
+export default HomeComponent;
