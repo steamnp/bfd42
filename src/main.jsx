729510acf87d854ab048707193c49742f9483077
Gorakh Raj Joshi <59221046+gorakhjoshi@users.noreply.github.com>
2023-12-29 08:50:27 +0545

    create only home component

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Home from './pages/home/home'
+import Home from './home'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
