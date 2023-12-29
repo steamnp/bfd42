10246879d7d3917c6b3a2acd73df5219e425a6a1
Aadam Rashid Malik <aadammalik465@gmail.com>
2023-12-28 19:59:28 -0600

    Updated function variable name

--- src/utils/number.js
+++ src/utils/number.js
@@ -4,6 +4,6 @@ function sumFunction(firstNumber) {
   return 20 + firstNumber;
 }
 
-const firstNum = sumFunction(21);
+const addNum = sumFunction(21);
 
-return <main className={StyleSheet.box}>firstNum</main>;
+return <main className={StyleSheet.box}>addNum</main>;
