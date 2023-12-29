52418df313a684a4df25ba8d23748df0501cc6b2
Sapna Tamang <tamangsapna08@gmail.com>
2023-12-28 19:31:19 -0600

    sum function

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 function App() {
-  return <div>App</div>;
+  return (
+    <img src="https://smarthistory.org/wp-content/uploads/2023/01/52108292698_9241f71354_6k-1536x947.jpg" />
+  );
 }
 
 export default App;
