e92688246a5fd09239fc0c3fd22c41fa624c39a4
Aadam Rashid Malik <aadammalik465@gmail.com>
2023-12-28 19:49:19 -0600

    functions to add two numbers

---
+++ src/utils/number.js
@@ -0,0 +1,9 @@
+import "./App.css";
+
+function sumFunction(firstNumber) {
+  return 20 + firstNumber;
+}
+
+const firstNum = sumFunction(21);
+
+return <main className={StyleSheet.box}>firstNum</main>;
