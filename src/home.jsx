10ce7d4ab31134aa0b0eb7e116558c38bc659a55
Gorakh Raj Joshi <59221046+gorakhjoshi@users.noreply.github.com>
2023-12-29 09:18:18 +0545

    values in javascript

--- src/home.jsx
+++ src/home.jsx
@@ -1,5 +1,32 @@
 function Home() {
-  return <div>Home</div>
+  // typeof myName -> string
+  const myName = 'Gorakh'
+
+  // typeof myFavNum -> number
+  const myFavNum = 10.01
+  let age = 20
+
+  // Array
+  // typeof myFavNumbers -> object (Array)
+  const myFavNumbers = [10, 20, 30]
+  const anotherArray = [10, 20, 30]
+
+  // typeof participant -> object
+  const participant = {
+    name: 'Rajiv',
+    favNumber: 7,
+  }
+
+  // typeof randomValue -> undefined
+  let randomValue
+
+  // typeof anotherValue -> object
+  const anotherValue = null
+
+  // typeof isProductAvailable -> boolean
+  const isProductAvailable = false
+
+  return <div>{typeof myFavNumbers}</div>
 }
 
 export default Home
