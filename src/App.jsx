785e630109334c71bcfa729f2937ef5eb5f5346e
Rajip Bardewa <rajip@takeo.ai>
2023-12-29 07:17:17 +0545

    clean up

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,5 @@
-import styles from "./App.module.css";
-
-// Component
-// if name is following PascalCase convention
-// & if it is returning JSX
 function App() {
-  return <main className={StyleSheet.box}>Hello</main>;
+  return <div>App</div>;
 }
 
 export default App;
-
-// function (generic JS function)
-// function getAge(){
-// return 20
-// }
-
-// let, const -> 2015
-
-// ED6 -> 2015 -> Modern JS
-// ES6(JS) modules
-// import
-// export
