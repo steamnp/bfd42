40c1803e991155c7682a81d591941e1600bfbaac
Lama <txl3311@gmail.com>
2023-12-28 20:31:44 -0600

    made css changes to the body

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -2,7 +2,7 @@ import sum from "../../utils/number";
 
 function Home() {
   const add = sum(2, 3);
-  return <div>the sum is {add}</div>;
+  return <div>the sum is :{add}</div>;
 }
 
 export default Home;
