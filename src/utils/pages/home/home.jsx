34a3017af88f6a5affc34fc878c3d44849917af6
sushant lamichhane <lamichhanesushant12@gmail.com>
2023-12-28 18:09:24 -0800

    create home component

--- src/utils/pages/home/home.jsx
+++ src/utils/pages/home/home.jsx
@@ -1,11 +1,4 @@
-import React from "react";
-
-const HomeComponent = () => {
-  return (
-    <div>
-      <h1>Welcome to our Website</h1>
-    </div>
-  );
-};
-
-export default HomeComponent;
+function Home() {
+  return <div>Home</div>;
+}
+export default Home;
