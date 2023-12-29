12b9c73ff44c05ad907d1b9c019aec9890286ad5
Aadam Rashid Malik <aadammalik465@gmail.com>
2023-12-28 20:02:38 -0600

    export function

--- src/utils/number.js
+++ src/utils/number.js
@@ -2,8 +2,9 @@ import "./App.css";
 
 function sumFunction(firstNumber) {
   return 20 + firstNumber;
+
+  const addNum = sumFunction(21);
+
+  return <main className={StyleSheet.box}>addNum</main>;
 }
-
-const addNum = sumFunction(21);
-
-return <main className={StyleSheet.box}>addNum</main>;
+export default sumFunction;
