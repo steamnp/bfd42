f6b968d41fa8234e534d861c6ba0be2d032df567
Lama <txl3311@gmail.com>
2023-12-29 19:44:05 -0600

    no specific changes

---
+++ src/home.jsx
@@ -0,0 +1,5 @@
+function Home() {
+  return <div>Home is sweet home</div>;
+}
+
+export default Home;
--- src/pages/home/home.jsx
+++
@@ -1,8 +0,0 @@
-import sum from "../../utils/number";
-
-function Home() {
-  const add = sum(2, 3);
-  return <div>the sum is :{add}</div>;
-}
-
-export default Home;
