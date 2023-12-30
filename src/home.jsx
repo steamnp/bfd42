cd260a8ae6a10305b207e967c24fc9d3c449de98
Gorakh Raj Joshi <59221046+gorakhjoshi@users.noreply.github.com>
2023-12-30 08:17:54 +0545

    discuss object destructuring

--- src/home.jsx
+++ src/home.jsx
@@ -1,47 +1,23 @@
 function Home() {
-  // typeof myName -> string
-  const myName = 'Gorakh'
-
-  // typeof myFavNum -> number
-  const myFavNum = 10.01
-  let age = 20
-
-  // Array
-  // typeof myFavNumbers -> object (Array)
-  const myFavNumbers = [10, 20, 30]
-  const anotherArray = [10, 20, 30]
-
-  // typeof participant -> object
-  const participant = {
-    name: 'Rajiv',
-    favNumber: 7,
+  const anotherValue = 10
+  // Object
+  const person = {
+    randomValue: {
+      newValue: 'Hello React',
+    },
+    anotherValue: 20,
   }
 
-  // typeof randomValue -> undefined
-  let randomValue
+  // Object destructuring
+  const {
+    anotherValue: diffValue,
+    randomValue: { newValue },
+  } = person
 
-  // typeof anotherValue -> object
-  const anotherValue = null
+  // const { newValue } = randomValue
 
-  // typeof isProductAvailable -> boolean
-  const isProductAvailable = false
-
-  const numberOne = 10
-  const numberTwo = 20
-
-  // Truthy Values
-
-  // Falsy values
-  // false, undefined, 0, '', null, NaN
-
-  // conditional statement
-  if (numberOne > numberTwo) {
-    console.log('I am truthy value!')
-  } else {
-    console.log('I am a falsy value!')
-  }
-
-  return <div>{typeof myFavNumbers}</div>
+  // You can only write javascript expression in JSX location
+  return <div>{diffValue}</div>
 }
 
 export default Home
