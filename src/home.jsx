9a133aa135e8d5d836962e43476c37c4b828e314
Lama <txl3311@gmail.com>
2023-12-29 20:15:56 -0600

    making object and destructuring it

--- src/home.jsx
+++ src/home.jsx
@@ -1,5 +1,30 @@
 function Home() {
-  return <div>Home is sweet home</div>;
+  // Object
+  const person = {
+    randomValue: {
+      newValue: "Hello React",
+    },
+    anotherValue: 100,
+  };
+
+  // Object destructuring
+  const {
+    randomValue: { newValue },
+    anotherValue,
+  } = person;
+  //const { newValue } = randomValue;
+
+  const arr = [10, 20];
+
+  return (
+    <>
+      <div>{person.anotherValue}</div>
+      {/* <div>{person.randomValue}</div> */}
+      <div>{newValue}</div>
+      <div>{arr}</div>
+      <div>{arr[1]}</div>
+    </>
+  );
 }
 
 export default Home;
