b17278ff00badf502221db2c66f35e4963a7c49b
Gorakh Raj Joshi <59221046+gorakhjoshi@users.noreply.github.com>
2023-12-30 08:24:26 +0545

    use of object destructuring

--- src/home.jsx
+++ src/home.jsx
@@ -17,6 +17,16 @@ function Home() {
   // const { newValue } = randomValue
 
   // You can only write javascript expression in JSX location
+
+  // input value name -> parameters
+  // object destructuring at parameter location
+  function add({ numberTwo, numberOne, numberThree }) {
+    return numberOne + numberTwo
+  }
+
+  // real input value -> arguments
+  // Object literal as input
+  add({ numberOne: 10, numberTwo: 20, numberThree: 40 })
   return <div>{diffValue}</div>
 }
 
