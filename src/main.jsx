7012ef9ff87655906891647f6bbb369bc52830ad
Gorakh Raj Joshi <59221046+gorakhjoshi@users.noreply.github.com>
2024-01-03 08:37:44 +0545

    create components and pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,4 @@
-// import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Home from './home'
+import App from './app'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-  <Home />
-  // </React.StrictMode>
-)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
