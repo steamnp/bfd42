c3bf883a8da2bafd43fae41d25a9f98b06f8ffb2
Gorakh Raj Joshi <59221046+gorakhjoshi@users.noreply.github.com>
2024-01-03 08:37:44 +0545

    create components and pages

---
+++ src/app.jsx
@@ -0,0 +1,17 @@
+import Footer from './components/footer'
+import Header from './components/header'
+import Home from './pages/home'
+import Service from './pages/service'
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Home />
+      <Service />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
