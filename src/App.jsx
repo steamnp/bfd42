3fd7e2646c92e6b3de4719c84f514311d125ca17
Sapna Tamang <tamangsapna08@gmail.com>
2024-01-03 18:55:45 -0600

    Card

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import React from "react";
-import "./global.css";
+import "./App.css";
 import Card from "./Components/Card";
 
 function App() {
   return (
     <div className="App">
-      <Hello
-        World
-        title="Card Title"
-        imgUrl="https://github.com/steamnp/bfd42/blob/assignment-1/design/active-states.jpg"
-        body="Mt Everest is the highest mountain in the entire world with 8849 M above the sea level which lies in the Solukhumbu District of Nepal"
+      <Card
+        title="card title"
+        imageUrl="https://github.com/steamnp/bfd42/blob/assignment-1/design/active-states.jpg"
+        body="The quick brown fox jumps over the lazy dog"
       />
     </div>
   );
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,12 +1,7 @@
 import React from "react";
-function Card({ title, imgUrl, body }) {
-  return (
-    <div className="card-container">
-      <div className="img-container">
-        <img src={imgUrl} alt="" />
-      </div>
-    </div>
-  );
+
+function Card() {
+  return <div className="card-container">card</div>;
 }
 
 export default Card;
