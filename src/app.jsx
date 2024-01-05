87a1e881658577665945eade26613e248c747ea8
Gorakh Raj Joshi <59221046+gorakhjoshi@users.noreply.github.com>
2024-01-05 08:45:27 +0545

    event handling in react

--- src/app.jsx
+++ src/app.jsx
@@ -1,18 +1,44 @@
+import styles from './app.module.css'
+
+console.log(styles)
+
 function App() {
-  const button = <button>Submit</button>
+  function handleClick() {
+    console.log('You clicked!')
+  }
 
-  // React Sunthetic Events
-  // onClick
-  // onChange
-  // onSubmit
+  function handleChangeEmail() {
+    console.log('You typed something in email field!')
+  }
 
-  console.log(button)
+  function handleChangePassword() {
+    console.log('You typed something in password field!')
+  }
 
   return (
-    <div>
-      <button>Submit</button>
+    <div className={`${styles.wrapper} ${styles.test}`}>
+      <div className={styles.container}>
+        <input onChange={handleChangeEmail} placeholder="email" type="text" />
+        <input onChange={handleChangePassword} placeholder="password" type="password" />
+      </div>
+      <div className={styles.btn}>
+        <button onClick={handleClick}>Login</button>
+      </div>
     </div>
   )
 }
 
 export default App
+
+// How to use events in JSX
+// What are some popular event?
+// what is the syntax for event name in HTML and JSX? -> Are they same?
+// HTML -> onchange, onclick
+// JSX -> onChange, onClick
+
+// What is the value type for event prop in JSX?
+// function -> callback function
+
+// If you have function written outside, how to use with events?
+// handleClick() x
+// handleClick
