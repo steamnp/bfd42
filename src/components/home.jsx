1ee8fda50bd72e870e177ce23b0b328bd9c7881d
Sanjip K Sapkota <sapkotasanjipk@outlook.com>
2024-01-06 00:53:10 -0500

    form fix continue

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,30 +1,39 @@
 import React from "react";
 import sytles from "./home.module.css";
+import { AuthLogo } from "../assets/authentication";
+
 
 export default function Home() {
   return (
+    <div classname={sytles.layout}>
     <div classname={sytles.container}>
     <form className={sytles.form}>
-      <div className={sytles.container}>
+      <div >
+        <div className={sytles.img}>
+   <AuthLogo/>
+   </div>
         <div  className={sytles.inputemail}>
-          <label htmlFor="name">User Name:</label>
+          <label htmlFor="email">Email:</label>
           <input
            
             type="text"
-            id="username"
-            name="name"
+            id="email"
+            name="email"
           />
         </div>
         <div className={sytles.inputpassword}>
-          <label htmlFor="name">Password:</label>
+          <label htmlFor="password">Password:</label>
           <input
             type="password"
             id="password"
             name="password"
           />
         </div>
+        <div className = {sytles.greenbtn}type='button'> Sign In</div>
       </div>
+      
     </form>
+    </div>
     </div>
   );
 }
