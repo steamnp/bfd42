30e782eb6287183ba91dfde58331b6a1c124b266
Gorakh Raj Joshi <59221046+gorakhjoshi@users.noreply.github.com>
2024-01-09 07:38:14 +0545

    functions in javascript

--- src/app.jsx
+++ src/app.jsx
@@ -1,18 +1,33 @@
-import styles from './app.module.css'
-
 function App() {
-  const names = ['Alex', 'Adam', 'Nima']
+  let email
+
   return (
-    <div>
-      {names.map(function (el, i) {
-        return (
-          <div className={styles.name} key={i}>
-            {el}
-          </div>
-        )
-      })}
-    </div>
+    <article>
+      <label htmlFor="">Email</label>
+      <input onChange={function () {}} type="text" />
+      <h3>You typed: </h3>
+    </article>
   )
 }
 
 export default App
+
+// create function in 3 ways
+// Function declaration
+function addFnDl(a, b) {
+  return a + b
+}
+
+addFnDl(10, 20)
+
+// Function expression
+const addFnEx = function (a, b) {
+  return a + b
+}
+
+addFnEx(10, 20)
+
+// Arrow function
+const addFnArr = (a, b) => a + b
+
+addFnArr(10, 20)
