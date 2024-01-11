8c38ddeb7b4a2c6aef086c06bc40d2ce9d579300
Lama <txl3311@gmail.com>
2024-01-10 19:02:34 -0600

    make form more responsive using css and react usestte hook

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,10 @@
+import { useState } from "react";
 import styles from "./app.module.css";
 
 function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <div className={styles.container}>
       <article className={styles.card}>
@@ -8,11 +12,30 @@ function App() {
         <div className={styles.credential}>
           <div className={styles.alignVertical}>
             <label htmlFor="email">Email:</label>
-            <input className={styles.input} type="email" id="email" />
+            <input
+              className={styles.input}
+              type="email"
+              id="email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </div>
           <div className={styles.alignVertical}>
             <label htmlFor="password">Password:</label>
-            <input className={styles.input} type="password" id="password" />
+            <input
+              className={`${styles.input} ${
+                password.length > 8 ? "" : styles.errorBorder
+              }`}
+              type="password"
+              id="password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            {password.length >= 8 ? (
+              ""
+            ) : (
+              <small className={styles.error}>
+                Your password is less than 8 characters.
+              </small>
+            )}
           </div>
           <button className={styles.btn}>Sign In</button>
         </div>
