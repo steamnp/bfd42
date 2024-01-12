445196e59471d415ada903c233eb3dbc4aaa0428
Nima Sherpa <sdorje40@gmail.com>
2024-01-12 15:28:39 -0500

    Imported sum function and rendered result in home componenet and app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+// src/app.jsx
 import React from "react";
+import Home from "./pages/home/home";
 
-function App() {
-  return <div>App</div>;
-}
+const App = () => {
+  return (
+    <div>
+      <h1>Your App</h1>
+      <Home />
+    </div>
+  );
+};
 
 export default App;
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { sum } from "../../utils/number";
 
 const Home = () => {
+  const result = sum(37, 10);
   return (
     <div>
-      <h1>Home Component</h1>
+      <h1>Sum of Two Numbers</h1>
+      <p>Result: {result}</p>
     </div>
   );
 };
