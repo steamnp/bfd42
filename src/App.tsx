aac9400db345a2c77a132d1958c3b1402036c047
Rakshyak Adhikari <rakshyak.adhikari@gmail.com>
2024-01-11 19:32:53 -0600

    initialized a typescript project

---
+++ src/App.tsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import styles from "./app.module.css";
+
+function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // Initial run -> render
+  // Again run -> re-render
+
+  // statement
+  // if(email) {
+  //   ''
+  // } else {
+  //   styles.errorBorder
+  // }
+
+  // expression
+  // ternary operator
+  // email ? styles.errorBorder : ''
+
+  return (
+    <div className={styles.container}>
+      <article className={styles.card}>
+        <img src="/icons/authentication.png" alt="" className={styles.img} />
+        <div className={styles.credential}>
+          <div className={styles.alignVertical}>
+            <label htmlFor="email">Email</label>
+            <input
+              onChange={(e) => setEmail(e.target.value)}
+              className={styles.input}
+              id="email"
+              type="email"
+            />
+          </div>
+          <div className={styles.alignVertical}>
+            <label htmlFor="password">Password</label>
+            <input
+              onChange={(e) => setPassword(e.target.value)}
+              className={`${styles.input} ${
+                password.length >= 8 ? "" : styles.errorBorder
+              }`}
+              id="password"
+              type="password"
+            />
+            {password.length >= 8 ? (
+              ""
+            ) : (
+              <small className={styles.error}>
+                Your password is less than 8 characters
+              </small>
+            )}
+          </div>
+          <button className={styles.btn}>Sign In</button>
+        </div>
+      </article>
+    </div>
+  );
+}
+
+export default App;
--- src/sign-in.jsx
+++
@@ -1,6 +0,0 @@
-function SignIn() {
-  return;
-  <div>SignIn</div>;
-}
-
-export default SignIn;
