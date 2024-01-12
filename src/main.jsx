d010f094d706a37c4070395fe7fe26262614e6ae
sushant lamichhane <lamichhanesushant12@gmail.com>
2024-01-11 18:02:50 -0800

    intialize type script

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Home from "./utils/pages/home/home.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Home />
-  </React.StrictMode>
-);
--- src/utils/number.js
+++
@@ -1,5 +0,0 @@
-function getSum(firstNum, secondNum) {
-  return firstNum + secondNum;
-}
-
-export default getSum;
