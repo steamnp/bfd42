b801356132635ebf094653839534e56fa7812cdc
Rakshyak Adhikari <rakshyak.adhikari@gmail.com>
2024-01-12 19:08:19 -0600

    added react-router-dom and styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-import styles from './app.module.css'
+import { useState } from "react";
+import styles from "./app.module.css";
 
 function App() {
-  const [, setEmail] = useState('')
+  const [, setEmail] = useState("");
 
-  const [password, setPassword] = useState('')
+  const [password, setPassword] = useState("");
 
   return (
     <div className={styles.container}>
@@ -13,18 +13,34 @@ function App() {
         <div className={styles.credential}>
           <div className={styles.alignVertical}>
             <label htmlFor="email">Email</label>
-            <input onChange={(e) => setEmail(e.target.value)} className={styles.input} id="email" type="email" />
+            <input
+              onChange={(e) => setEmail(e.target.value)}
+              className={styles.input}
+              id="email"
+              type="email"
+            />
           </div>
           <div className={styles.alignVertical}>
             <label htmlFor="password">Password</label>
-            <input onChange={(e) => setPassword(e.target.value)} className={`${styles.input} ${password.length >= 8 ? '' : styles.errorBorder}`} id="password" type="password" />
-            {password.length >= 8 ? '' : <small className={styles.error}>Your password is less than 8 characters</small>}
+            <input
+              onChange={(e) => setPassword(e.target.value)}
+              className={`${styles.input} ${
+                password.length >= 8 ? "" : styles.errorBorder
+              }`}
+              id="password"
+              type="password"
+            />
+            {password.length >= 8 || (
+              <small className={styles.error}>
+                Your password is less than 8 characters
+              </small>
+            )}
           </div>
           <button className={styles.btn}>Sign In</button>
         </div>
       </article>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
