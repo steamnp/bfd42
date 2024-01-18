30e28536ddd97733dffc542a4adfbc9731d13c3b
Gorakh Raj Joshi <59221046+gorakhjoshi@users.noreply.github.com>
2024-01-18 08:39:31 +0545

    add global css

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,12 +1,14 @@
 import { Outlet, createBrowserRouter } from 'react-router-dom'
 import Home from '../pages/home'
 import Contact from '../pages/contact'
+import { GlobalStyles } from '../style/global-style'
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: (
       <>
+        <GlobalStyles />
         <div>Header</div>
         <Outlet />
         <div>Footer</div>
