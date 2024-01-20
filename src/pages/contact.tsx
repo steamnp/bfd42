c19cd089251a2c788e69a5dab2793ac49f8b8de5
Aadam Rashid Malik <aadammalik465@gmail.com>
2024-01-19 20:28:59 -0600

    intialize footers

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,5 +1,15 @@
 function Contact() {
-  return <div>Contact </div>;
+  return (
+    <div>
+      Contact<h1>About Us</h1>
+      <p>
+        Ready to take it to the next level? Let’s talk about your project or
+        idea and find out how we can help your business grow. If you are looking
+        for unique digital experiences that’s relatable to your users, drop us a
+        line.
+      </p>
+    </div>
+  );
 }
 
 export default Contact;
