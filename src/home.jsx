207e0c2e2c6392917b174fadd6d322e0f07dd148
RajiB <rajip@takeo.ai>
2024-01-22 17:28:29 -0500

    values in javascript

--- src/home.jsx
+++ src/home.jsx
@@ -1,5 +1,31 @@
 function home() {
-  return <div>Home</div>;
+  // typeof myName -> string
+  const myName = "Rajip";
+
+  // typeof myFavNum -> number
+  const myFavNum = 7;
+  let age = 26;
+
+  //Array
+  // typeof myFavNumbers -> object (Array)
+  const myFavNumbers = [10, 20, 30];
+
+  // typeof myFavNumbers -> object
+  const participant = {
+    name: "Gorakh",
+    favNumber: 10,
+  };
+
+  // typeof randomValue -> undefined
+  let randomValue;
+
+  // valueof anotherValue -> object
+  const anotherValue = null;
+
+  // typeof isProductAvailable -> boolen
+  const isProductAvailable = false;
+
+  return <div>{typeof isProductAvailable}</div>;
 }
 
 export default home;
