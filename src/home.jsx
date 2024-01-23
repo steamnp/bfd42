66f18f2d2d521ff5527571a4cc4e492c7e262155
RajiB <rajip@takeo.ai>
2024-01-23 05:09:10 -0500

    pass prop from parent to children component

--- src/home.jsx
+++ src/home.jsx
@@ -1,33 +1,20 @@
+import Header from "./header";
+
 function Home() {
-  const anotherValue = 10;
-  // object
-  const person = {
-    randomValue: {
-      newValue: "Hello React",
-    },
-    anotherValue: 20,
-  };
+  return (
+    <div>
+      {/* headerText -> props */}
+      {/* subTitle -> props*/}
+      <Header subTitle="I am subtitle!">
+        <h1>Hello React</h1>
+        <h2>React Component</h2>
+      </Header>
 
-  // Object destructuring
-  const {
-    anotherValue: diffValue,
-    randomValue: { newValue },
-  } = person;
-
-  // const { newValue } = randomValue;
-
-  // you can only write javascript expression in JSX location
-
-  // input value name -> paramenters
-  // object distructuring at parameter location
-  function add({ numberTwo, numberOne, numberThree }) {
-    return numberOne + numberTwo;
-  }
-
-  //real input value -> arguments
-  // object literal as input
-  add({ numberOne: 10, numberTwo: 20, numberThree: 40 });
-  return <div>{diffValue}</div>;
+      <Header>
+        <h1>Hello JS</h1>
+      </Header>
+    </div>
+  );
 }
 
 export default Home;
