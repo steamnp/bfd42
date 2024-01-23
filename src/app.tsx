f185670f9af5ec3db296771df9c2aa2c0c050fba
Gorakh Raj Joshi <59221046+gorakhjoshi@users.noreply.github.com>
2024-01-23 08:24:26 +0545

    useEffect hook

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,26 @@
+import { useEffect } from 'react'
+
 function App() {
+  console.log('1')
+  // There are 2 inputs that we can pass in useEffect hook
+  // 1st input is compulsory -> callback function
+  // callback function -> input/output
+  // 2nd input is optional -> [] -> Dependency Array
+
+  // [1,2,3].map(() =>  {})
+
+  // useEffect hook will always be callled/invoked at the end of component rendering
+
+  const callbackFn = () => {
+    console.log('2')
+  }
+
+  console.log('3')
+
+  useEffect(callbackFn)
+
+  console.log('4')
+
   return <div>App</div>
 }
 
