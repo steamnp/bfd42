8359ad51aeab593deb6501d3188a8b03ed86e37b
Sapna Tamang <tamangsapna08@gmail.com>
2024-01-23 20:42:53 -0600

    learn asyc codes

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,8 @@ function App() {
   console.log("Before function");
 
   async function getUsers() {
-    console.log("Before Await");
-
-    const data = await fetch("https://api.github.com/users");
-
-    console.log("After await");
+    const output = fetch("https://api.github.com/users");
+    console.log(output);
   }
   getUsers();
   return <div>App</div>;
