58d37e6010a8b28c6e769402eb5f9e6dc3e0f702
RajiB <rajip@takeo.ai>
2024-01-26 03:08:47 -0500

    event handling in react

--- src/app.jsx
+++ src/app.jsx
@@ -1,14 +1,33 @@
+import styles from "./app.module.css";
+
+console.log(styles);
+
 function App() {
-  const arr = [1, 2, 3, 4, 5];
+  function handleClick() {
+    console.log("You Clicked!");
+  }
 
-  const randomArr = [];
+  function handleChangeEmail() {
+    console.log("You typed something in email field!");
+  }
 
-  console.log(arr);
-  console.dir(document);
-
-  console.log(null);
-
-  return <div>App</div>;
+  function handleChangePassword() {
+    console.log("You typed something in password field!");
+  }
+  return (
+    <div className={styles.wrapper}>
+      <div className={"$styles.container} ${styles.test}"}>
+        <input onChange={handleChangeEmail} placeholder="email" type="text" />
+        <input
+          onChange={handleChangePassword}
+          placeholder="password"
+          type="password"
+        />
+      </div>
+      <div className={styles.btn}>
+        <button onClick={handleClick}>Login</button>
+      </div>
+    </div>
+  );
 }
-
 export default App;
