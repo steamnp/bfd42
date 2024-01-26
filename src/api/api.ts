ac8a2a79d4c8f0d60a210e216fbeeaf4ef9a6c26
Aadam Rashid Malik <aadammalik465@gmail.com>
2024-01-25 21:10:52 -0600

    Updating api url

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,19 +1,19 @@
-import { IUser } from '../type/api'
+import { IGithubUserName } from "../type/api";
 
 export async function getUsers() {
   // Use try and catch block to hangle error in async await function
   try {
-    const output = await fetch(import.meta.env.VITE_API_URL)
-    const data: IUser[] = await output.json()
-
-    return data
+    const output = await fetch(import.meta.env.VITE_API_URL);
+    const data: IGithubUserName = await output.json();
+    console.log(data);
+    return data;
   } catch (error) {
     if (error instanceof Error) {
       // If the error is an instance of Error, you can access its properties
-      throw new Error(error.message)
+      throw new Error(error.message);
     } else {
       // If it's not an instance of Error, you can handle it accordingly
-      throw new Error('An unknown error occurred')
+      throw new Error("An unknown error occurred");
     }
   }
 }
