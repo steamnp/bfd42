321a4deb0e3875e5a9ff7ba32ea72b558b629740
Lama <txl3311@gmail.com>
2024-01-26 20:51:50 -0600

    tailwind css tutorial

--- src/component/app.tsx
+++ src/component/app.tsx
@@ -1,18 +1,18 @@
-import { useEffect, useState } from 'react'
-import { getUsers } from '../api/api'
+import { useEffect, useState } from "react";
+import { getUsers } from "../api/api";
 
-import { IGithubUserName } from '../type/api'
-import { Container, Error, Heading, Image } from '../style/style'
+import { IGithubUserName } from "../type/api";
+import { Container, Error, Heading, Image } from "../style/style";
 
 function App() {
-  const [user, setUser] = useState<IGithubUserName>()
-  const [error, setError] = useState<Error | undefined>(undefined)
+  const [user, setUser] = useState<IGithubUserName>();
+  const [error, setError] = useState<Error | undefined>(undefined);
 
   useEffect(() => {
     getUsers()
       .then((data) => setUser(data))
-      .catch((error) => setError(error))
-  }, [])
+      .catch((error) => setError(error));
+  }, []);
 
   return (
     <div>
@@ -25,7 +25,7 @@ function App() {
         </Container>
       )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
