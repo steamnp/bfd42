5fca5a5a1366871b0822560f3b7a78f288d759c4
Lama <txl3311@gmail.com>
2024-01-29 20:30:52 -0600

    no changes

--- src/style/style.ts
+++ src/style/style.ts
@@ -1,12 +1,12 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const Error = styled.div`
   color: red;
-`
+`;
 
 export const Image = styled.img`
   border-radius: 50%;
-`
+`;
 
 export const Container = styled.div`
   height: 100vh;
@@ -14,8 +14,8 @@ export const Container = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-`
+`;
 
 export const Heading = styled.h2`
   color: navy;
-`
+`;
