56ed6864eefddb7c677b27c2adaa11f21c3fab29
sushant lamichhane <lamichhanesushant12@gmail.com>
2024-01-29 19:56:53 -0800

    button setup

---
+++ src/App.ts
@@ -0,0 +1,12 @@
+import GlobalStyle from "./styles/GlobalStyle";
+import { DarkButton, LightButton } from "./styles/Buttons";
+
+function App() {
+  return (
+    <div className="App">
+      <GlobalStyle />
+      <DarkButton>Learn More</DarkButton>
+      <LightButton>Learn More</LightButton>
+    </div>
+  );
+}
