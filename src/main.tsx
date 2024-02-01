3d36cddd8a4bc4f6b2ae1c1b57318fad20d028a5
Sanjip K Sapkota <sapkotasanjipk@outlook.com>
2024-01-31 21:40:14 -0500

    pass store to top level component

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,9 @@ import { Provider } from 'react-redux'
 import store from './redux/store/store'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+
+<Provider store = {store}>
+<App />
+
+</Provider>
 )
