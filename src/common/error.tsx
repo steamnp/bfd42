7ac7113c87df2e627b872c97a5e1a57d7631c986
Nima Sherpa <sdorje40@gmail.com>
2024-02-01 18:10:06 -0500

    steup error page and outlet inside router.tsx

---
+++ src/common/error.tsx
@@ -0,0 +1,5 @@
+function Error() {
+  return <div>Unable to load this page !</div>;
+}
+
+export default Error;
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,20 +1,37 @@
-import { createBrowserRouter } from "react-router-dom";
+import { Outlet, createBrowserRouter } from "react-router-dom";
 import Home from "../pages/home";
 import Store from "../pages/store";
 import Contact from "../pages/contact";
+import Error from "../common/error";
+import Header from "../common/header";
+import Footer from "../common/footer";
 
 export const router = createBrowserRouter([
   {
-    path: "/store",
-    element: <Store />,
-  },
-  {
     path: "/",
-    element: <Home />,
-  },
+    element: (
+      <>
+        <Header />
+        <Outlet />
+        <Footer />
+      </>
+    ),
+    errorElement: <Error />,
 
-  {
-    path: "/contact",
-    element: <Contact />,
+    children: [
+      {
+        path: "/home",
+        element: <Home />,
+      },
+      {
+        path: "/store",
+        element: <Store />,
+      },
+
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
   },
 ]);
