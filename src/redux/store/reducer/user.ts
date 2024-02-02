dbca8b9354865d3809dd96d5307375ebf02c864d
Aadam Rashid Malik <aadammalik465@gmail.com>
2024-02-01 19:54:09 -0600

    create reducer file and updated store folder

---
+++ src/redux/store/reducer/user.ts
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState: [{ name: "user 1" }, { name: "name 2" }],
+  reducers: {
+    // method (function) 1
+    login: () => {},
+
+    // method (function) 2
+    logout: () => {},
+    // method (function) 3
+    // method (function) 4
+  },
+});
+
+// We will use action from View (UI or Component)
+export const { login, logout } = userSlice.actions;
+
+// We need to link below reducer to store
+export default userSlice.reducer;
