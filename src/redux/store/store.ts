8ab3336d2262d5d6fbf03cfc17746e4d1060fb4c
Nima Sherpa <sdorje40@gmail.com>
2024-02-05 21:33:06 -0500

    use autSlice with redux store

--- src/redux/store/store.ts
+++ src/redux/store/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "../reducer/authSlice";
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    auth: authReducer,
+  },
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
