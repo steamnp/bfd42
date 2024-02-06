6a7c3421c5a11d583000cf744dbed6d422c40161
Sanjip K Sapkota <sapkotasanjipk@outlook.com>
2024-02-05 21:54:07 -0500

    fix file structure for redux

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom/client";
 import App from "./app";
 import { Provider } from "react-redux";
-import store from "./store/store";
+import store from "./redux/store/store";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,19 +1,15 @@
-import { Form } from "./login.style"
-
+import { Form } from "./login.style";
 
 const Login = () => {
-    
-
-
   return (
     <Form>
-<label htmlFor="username">User Name</label>
-<input type='email' name='username' id='username' required></input>
-<label htmlFor="password">Password</label>
-<input type='password' id = 'passord' required></input>
-<button>Login</button>
-</Form>
-  )
-}
+      <label htmlFor="username">User Name</label>
+      <input type="email" name="username" id="username" required></input>
+      <label htmlFor="password">Password</label>
+      <input type="password" id="passord" required></input>
+      <button>Login</button>
+    </Form>
+  );
+};
 
-export default Login
+export default Login;
