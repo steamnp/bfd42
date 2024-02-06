1ee013be9715caeba5f268f78c00e337c60f2674
Sanjip K Sapkota <sapkotasanjipk@outlook.com>
2024-02-05 23:33:30 -0500

    create login compt and state

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,12 +1,32 @@
+import { useState } from "react";
 import { Form } from "./login.style";
 
+interface IUser {
+email: string;
+password: string;
+}
+
 const Login = () => {
+  const [inputs, setInputs] = useState<IUser>({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (event:React.ChangeEvent<HTMLInputElement>) =>{
+    setInputs({...inputs, 
+       [event.target.name]:event.target.value})
+  }
+
+  const handleSumbit = () =>{
+    alert('submitted')
+  }
+  
   return (
-    <Form>
-      <label htmlFor="username">User Name</label>
-      <input type="email" name="username" id="username" required></input>
+    <Form onSubmit = {handleSumbit}>
+      <label htmlFor="email">User Email</label>
+      <input type="email" name="email" id="email" required onChange = {handleChange}/>
       <label htmlFor="password">Password</label>
-      <input type="password" id="password" name='password' required></input>
+      <input type="password" id="password" name="password" required onChange ={handleChange}/>
       <button>Login</button>
     </Form>
   );
