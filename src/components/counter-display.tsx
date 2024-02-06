fe36dde0f0c7f699940cac9a41a7c1731cb0bf4e
Sanjip K Sapkota <sapkotasanjipk@outlook.com>
2024-02-05 20:26:00 -0500

    initialize react

--- src/components/counter-display.tsx
+++
@@ -1,8 +0,0 @@
-import { useAppSelector } from '../redux/hooks/hooks'
-
-function CounterDisplay() {
-  const count = useAppSelector((state) => state.user)
-  return <h1>{count}</h1>
-}
-
-export default CounterDisplay
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
+import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './app'
-import { Provider } from 'react-redux'
-import store from './redux/store/store'
+import App from './App.tsx'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <Provider store={store}>
+  <React.StrictMode>
     <App />
-  </Provider>
+  </React.StrictMode>,
 )
