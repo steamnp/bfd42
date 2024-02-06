3e77f5e056c3b0d74fed26e1501e384ddf87261d
Sanjip K Sapkota <sapkotasanjipk@outlook.com>
2024-02-05 20:56:49 -0500

    create authslice and update store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
-
+import authReducer from '../reducer/authSlice';
 
 const store = configureStore({
   reducer: {
-    
+    auth: authReducer,
   },
 })
 
