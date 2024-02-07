e106ce6067098294c64e5d8515347d44e85da167
Aadam Rashid Malik <aadammalik465@gmail.com>
2024-02-06 19:52:07 -0600

    create redux folder

---
+++ src/components/login.tsx
@@ -0,0 +1,5 @@
+function Login() {
+  return <div>User-Login</div>;
+}
+
+export default Login;
--- src/redux/store/store.ts
+++ src/redux/store/store.ts
@@ -1,11 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "../reducer/authSlice";
 
 const store = configureStore({
-  reducer: [],
+  reducer: {
+    auth: authReducer,
+  },
 });
 
+// Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
-
-export type AppDispatch = typeof store.getState;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;
 
 export default store;
