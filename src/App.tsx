3d6434aea1a400d02c796c0d4f0bc7d790748fd7
Lama <txl3311@gmail.com>
2024-02-06 19:31:10 -0600

    use useState for email

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 function App() {
-  return <div>Apppppppleeee</div>;
+  return <div>hello</div>;
 }
 
 export default App;
---
+++ src/components/login.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+
+function login() {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  return <div></div>;
+}
+
+export default login;
