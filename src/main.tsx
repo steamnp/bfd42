b151a38f18fa04f270c4229e3a3ec695bbec8a84
Aadam Rashid Malik <aadammalik465@gmail.com>
2024-02-06 18:08:52 -0600

    intialize react app

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import ReactDOM from "react-dom/client";
-import App from "./app";
-import { Provider } from "react-redux";
-import store from "./redux/store/store";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <Provider store={store}>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <App />
-  </Provider>
-);
+  </React.StrictMode>,
+)
