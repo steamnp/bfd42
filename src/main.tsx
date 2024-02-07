88f4124cbc6ef3b202ce86a0134e607792550abb
Aadam Rashid Malik <aadammalik465@gmail.com>
2024-02-06 18:52:36 -0600

    create reducer and hooks folder

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,11 @@
 import ReactDOM from "react-dom/client";
-import App from "./app.tsx";
+import App from "./app";
 import "./index.css";
 import { Provider } from "react-redux";
 import store from "./redux/store/store";
-import React from "react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
