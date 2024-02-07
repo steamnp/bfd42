b4bfe1ccf6aabb92dde925af5ff500f599ef0707
Pemba lama <lamapemba027@gmail.com>
2024-02-06 19:07:56 -0500

    create login compt and state

---
+++ src/pages/login.style.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Form = styled.form`
+width: 400px;
+display: flex;
+flex-direction: column;
+margin: auto;
+padding: 100px;
+
+`
---
+++ src/pages/login.tsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import { Form } from "./login.style";
+
+interface IUser {
+  email: string;
+  password: string;
+}
+
+const Login = () => {
+  const [inputs, setInputs] = useState<IUser>({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputs({ ...inputs, [event.target.name]: event.target.value });
+  };
+
+  const handleSumbit = () => {
+    alert("submitted");
+  };
+
+  return (
+    <Form onSubmit={handleSumbit}>
+      <label htmlFor="email">User Email</label>
+      <input
+        type="email"
+        name="email"
+        id="email"
+        required
+        onChange={handleChange}
+      />
+      <label htmlFor="password">Password</label>
+      <input
+        type="password"
+        id="password"
+        name="password"
+        required
+        onChange={handleChange}
+      />
+      <button>Login</button>
+    </Form>
+  );
+};
+
+export default Login;
