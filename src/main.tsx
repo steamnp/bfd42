3332cfb188533c5879cdd737f3b7d1b1e59c6f92
Gorakh Raj Joshi <59221046+gorakhjoshi@users.noreply.github.com>
2024-02-07 08:19:26 +0545

    create home page

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import ReactDOM from 'react-dom/client'
-import App from './app'
-import { Provider } from 'react-redux'
-import store from './redux/store/store'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.scss';
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './pages/home/home.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
-)
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
