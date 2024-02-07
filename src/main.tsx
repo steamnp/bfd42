8a37ca82904479fdd2b656aac80a581d8294bd1e
Aadam Rashid Malik <aadammalik465@gmail.com>
2024-02-06 18:27:40 -0600

    correct main.tsx code with correct provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import App from "./app.tsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import store from "./redux/store/store";
+import React from "react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
