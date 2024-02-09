6d94925c2aa3fe370469bb346f982fdf78e48c9a
Sanjip K Sapkota <sapkotasanjipk@outlook.com>
2024-02-09 15:05:05 -0500

    create api and env variballe

--- src/redux/reducer/api.ts
+++ src/redux/reducer/api.ts
@@ -1,19 +1,24 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { MovieData, TVShowData, UpcomingMoviesData } from '../../models';
 
+const API_KEY = import.meta.env.VITE_API_KEY;
+
+console.log('>>>>>' + API_KEY);
 // Define a service using a base URL and expected endpoints
 export const MovieDBApi = createApi({
   reducerPath: 'MovieDBAPI',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://api.themoviedb.org/' }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://api.themoviedb.org/3/'
+  }),
   endpoints: (builder) => ({
     getMovieData: builder.query<MovieData, void>({
-      query: () => `/`
+      query: () => `movie/popular?api_key=${API_KEY}`
     }),
     getTvShowData: builder.query<TVShowData, void>({
-      query: () => `/`
+      query: () => `trending/tv/day?api_key=${API_KEY}`
     }),
     getUpComingData: builder.query<UpcomingMoviesData, void>({
-      query: () => `/`
+      query: () => `movie/upcoming?api_key=${API_KEY}`
     })
   })
 });
